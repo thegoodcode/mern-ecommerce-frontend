23c22d22b208215e99bbb48cae5580efec7c1b13
thegoodcode <rhapsody@tuta.io>
2021-10-04 20:27:31 +0100

    add redux, react-redux, redux-thunk & redux-devtools-extensions libraries and simple store setup with provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './bootstrap.min.css'
 import './index.scss'
 import App from './App'
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary'
+import { Provider } from 'react-redux'
+import store from 'store'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<ErrorBoundary>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</ErrorBoundary>
 	</React.StrictMode>,
 	document.getElementById('root')
