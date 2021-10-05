e99f9f91a643e4fc2aaa13889ff58cf6e5350c70
thegoodcode <rhapsody@tuta.io>
2021-10-05 12:45:24 +0100

    add Qty select input in product screen

--- src/screens/ProductScreen/ProductScreen.js
+++ src/screens/ProductScreen/ProductScreen.js
@@ -2,13 +2,14 @@ import { listProductDetails } from 'actions/productActions'
 import Loader from 'components/Loader/Loader'
 import Message from 'components/Message/Message'
 import Rating from 'components/Rating/Rating'
-import { useEffect } from 'react'
-import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
+import { useEffect, useState } from 'react'
+import { Button, Card, Col, Form, Image, ListGroup, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import './ProductScreen.scss'
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ match, history }) => {
+	const [qty, setQty] = useState(1)
 	const dispatch = useDispatch()
 	const {
 		loading,
@@ -26,6 +27,9 @@ const ProductScreen = ({ match }) => {
 	useEffect(() => {
 		dispatch(listProductDetails(match.params.id))
 	}, [match, dispatch])
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`)
+	}
 	return (
 		<>
 			<Link to='/' className='btn btn-light my-3 border'>
@@ -69,8 +73,28 @@ const ProductScreen = ({ match }) => {
 										<Col>{countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
 									</Row>
 								</ListGroup.Item>
+								{countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as='select'
+													value={qty}
+													onChange={e => setQty(e.target.value)}>
+													{[...Array(countInStock).keys()].map(x => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 								<ListGroup.Item>
 									<Button
+										onClick={addToCartHandler}
 										className='btn-block add-button'
 										type='button'
 										disabled={countInStock === 0}>
